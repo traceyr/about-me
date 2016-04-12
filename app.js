85a79eaeab8fc27841307decfc8ae927797933fc
Tracey <radcliffe.tracey@gmail.com>
2016-04-12 14:23:45 -0700

    finished preliminary html

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var answerY = function(answer, sentence) {
   'use strict';
   if (answer.toUpperCase() === 'Y') {
     alert('You are correct, ' + sentence);
+    console.log(sentence);
   } else {
     alert('That is incorrect, ' + sentence);
+    console.log(sentence);
   }
 };
 
@@ -11,12 +13,16 @@ var answerN = function(answer, sentence) {
   'use strict';
   if (answer.toUpperCase() === 'N') {
     alert('You are correct, ' + sentence);
+    console.log(sentence);
   } else {
     alert('That is incorrect, ' + sentence);
+    console.log(sentence);
   }
 };
 
-alert('Hello, lets play this quiz to see how well you know me!');
+var name = prompt('What is your name?');
+console.log('Welcome ' + name + ' to my little quiz.');
+alert('Hello, ' + name + ' lets play this quiz to see how well you know me!');
 alert('For all of your answers, please answer with a simple Y or N');
 
 var answer1 = prompt('Do I have a cat named Minerva?');
@@ -32,7 +38,7 @@ var answer4 = prompt('Is this my first class at CodeFellows?');
 var q4 = answerN(answer4, 'I took classes last year!');
 
 var answer5 = prompt('Do I like to ski/snowboard?');
-var q5 = answerN(answer5, 'I dislike all sports that involve snow. Snow is cold.');
+var q5 = answerN(answer5, 'I dislike all sports that involve snow. Snow is cold. I dislike snow.');
 
 var answer6 = prompt('Did I train to be a Dietitian in college?');
 var q6 = answerY(answer6, 'I am a Registered Dietitian.');
