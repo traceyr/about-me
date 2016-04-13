7eaf447a9cf5b864dff53408c8d9c2e2f7831e61
Tracey <radcliffe.tracey@gmail.com>
2016-04-13 16:16:14 -0700

    before switching while and for

--- app.js
+++ app.js
@@ -38,36 +38,68 @@
 //
 // var answer5 = prompt('Did I train to be a Dietitian in college?');
 // var q5 = answerY(answer5, 'I am a Registered Dietitian.');
+//
+// alert('A different format for the next question. The rules here change a bit. For this next question, I will present the question and you will have four guesses to get it right. Please enter a numerical value between 1 - 20 Good Luck!');
+//
+// var qSixCounter = 1;
+// var max = 4;
+// var answer6;
+// var numJobs = 4;
+// while (qSixCounter <= max) {
+//   answer6 = prompt('How many jobs have I had?');
+//   if (isNaN(answer6) === false) {
+//     if (parseInt(answer6) === numJobs) {
+//       alert('Correct, I have had four jobs! You got it right on your attempt # ' + qSixCounter + '!' );
+//       console.log('Yah it only too you ' + qSixCounter + ' tries!');
+//       qSixCounter = max + 1;
+//     } else {
+//       if (parseInt(answer6) >= numJobs){
+//         alert('Incorrect. That was too high.');
+//       } else if (parseInt(answer6) <= numJobs) {
+//         alert('Incorrect. Too low.');
+//       }
+//       console.log('That was attempt # ' + qSixCounter + '. You have ' + (max - qSixCounter) + ' tries left.' );
+//       if (qSixCounter === max) {
+//         alert('Sorry, you did not quess correctly. I have had 4 jobs.');
+//         console.log('FAILLLLLLLLLLL');
+//       }
+//       qSixCounter++;
+//     }
+//   } else {
+//     alert('That is not a valid number. Please try again.');
+//     answer6;
+//   }
+// }
 
-alert('A different format for the next question. The rules here change a bit. For this next question, I will present the question and you will have four guesses to get it right. Please enter a numerical value between 1 - 20 Good Luck!');
+var countries = ['belize', 'france', 'germany', 'hungary', 'liechtenstein', 'italy', 'switzerland', 'poland'];
+var countriesCombined = countries.join(', ');
+var tries = 6;
+var qSevenCounter = 1;
+var sevenTester = false;
 
-var qSixCounter = 1;
-var max = 4;
-var answer6;
-var numJobs = 4;
-while (qSixCounter <= max) {
-  answer6 = prompt('How many jobs have I had?');
-  if (isNaN(answer6) === false) {
-    if (parseInt(answer6) === numJobs) {
-      alert('Correct, I have had four jobs! You got it right on your attempt # ' + qSixCounter + '!' );
-      console.log('Yah it only too you ' + qSixCounter + ' tries!');
-      qSixCounter = max + 1;
-    } else {
-      if (parseInt(answer6) >= numJobs){
-        alert('Incorrect. That was too high.');
-      } else if (parseInt(answer6) <= numJobs) {
-        alert('Incorrect. Too low.');
-      }
-      console.log('That was attempt # ' + qSixCounter + '. You have ' + (max - qSixCounter) + ' tries left.' );
-      if (qSixCounter === max) {
-        alert('Sorry, you did not quess correctly. I have had 4 jobs.');
-        console.log('FAILLLLLLLLLLL');
-      }
-      qSixCounter++;
-    }
-  } else {
-    alert('That is not a valid number. Please try again.');
-    answer6;
+alert('For this last question, please type in a word for your answer.');
+
+while (qSevenCounter <= tries) {
+  var answer7 = prompt('Name a country I have visited.').toLowerCase();
+  for (var i = 0; i <= countries.length; i++) {
+    console.log(countries[i]);
+  //   if (answer7 === countries[i]) {
+  //     alert('Correct. I have been to ' + answer7 + '. Here is a partial list of countries I have visited: ' + countriesCombined);
+  //     console.log(countriesCombined);
+  //     qSevenCounter = 7;
+  //     break;
+  //   } else {
+  //     alert('Incorrect. Please try again.');
+  //     console.log('Nope, you have ' + (tries - qSevenCounter) + ' left.');
+  //     qSevenCounter++;
+  //     // break;
+  //     if (qSevenCounter === tries) {
+  //       alert('Nope. Here is a partial list of countries I have visited: ' + countriesCombined);
+  //       console.log('FAILL');
+  //       // break;
+  //     }
+  //   }
+  //   // answer7;
   }
 }
 
